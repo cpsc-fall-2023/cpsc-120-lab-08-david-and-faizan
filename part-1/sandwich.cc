70abbb4a2be37a8101319c41730ae671943f4c91
David Perez <perez.david1@csu.fullerton.edu>
2023-10-30 14:35:03 -0700

    Fixed formatting errors.

--- part-1/sandwich.cc
+++ part-1/sandwich.cc
@@ -3,28 +3,26 @@
 // @najmeefaizan
 // Partners: @davidddd1
 
-
 #include <iostream>
 #include <string>
 #include <vector>
 
-
 int main(int argc, char* argv[]) {
- std::vector<std::string> arguments{argv, argv + argc};
+  std::vector<std::string> arguments{argv, argv + argc};
 
+  if (argc != 4) {
+    std::cerr << "Error: Invalid number of arguments. Please provide protein, "
+                 "bread, and condiment."
+              << std::endl;
+    return 1;
+  }
 
- if (argc != 4) {
-   std::cerr << "Error: Invalid number of arguments. Please provide protein, bread, and condiment." << std::endl;
-       return 1;
-   }
+  std::string protein = arguments[1];
+  std::string bread = arguments[2];
+  std::string condiment = arguments[3];
 
+  std::cout << "Your order: A " << protein << " sandwich on " << bread
+            << " with " << condiment << "." << std::endl;
 
-   std::string protein = arguments[1];
-   std::string bread = arguments[2];
-   std::string condiment = arguments[3];
-  
-   std::cout << "Your order: A " << protein << " sandwich on " << bread << " with " << condiment << "." << std::endl;
-
-
-   return 0;
+  return 0;
 }
