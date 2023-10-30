9fb6e246e4edad0bc6e351883937f5e1066518e9
David Perez <perez.david1@csu.fullerton.edu>
2023-10-30 14:17:57 -0700

    Still need to checkformat and lint errors

--- part-1/sandwich.cc
+++ part-1/sandwich.cc
@@ -1,23 +1,30 @@
-// TODO: Add the required header
+// Faizan Najmee
+// najmeefaizan@csu.fullerton.edu
+// @najmeefaizan
+// Partners: @davidddd1
+
 
 #include <iostream>
 #include <string>
 #include <vector>
 
+
 int main(int argc, char* argv[]) {
-  std::vector<std::string> arguments{argv, argv + argc};
+ std::vector<std::string> arguments{argv, argv + argc};
 
-  // TODO: Validate that the number of arguments is correct.
-  // If not, print an error message and return a non-zero value.
 
-  // TODO: Declare three std::string variables to hold the
-  // protein, bread, and condiment input.
-  // Initialize each variable with an element of the arguments vector
-  // declared above.
+ if (argc != 4) {
+   std::cerr << "Error: Invalid number of arguments. Please provide protein, bread, and condiment." << std::endl;
+       return 1;
+   }
 
-  // TODO: Use cout to print output following the pattern
-  // Your order: A *PROTEIN* sandwich on *BREAD* with *CONDIMENT*.
-  // on its own line.
 
-  return 0;
+   std::string protein = arguments[1];
+   std::string bread = arguments[2];
+   std::string condiment = arguments[3];
+  
+   std::cout << "Your order: A " << protein << " sandwich on " << bread << " with " << condiment << "." << std::endl;
+
+
+   return 0;
 }
